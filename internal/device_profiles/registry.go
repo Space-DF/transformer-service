af483120e3685310a2914ea86365ceee17e43e36
lethanhdat762003 <dat.le@digitalfortress.dev>
2026-04-09 15:16:40 +0700

    feat: integrate milesight am307

--- internal/device_profiles/registry.go
+++ internal/device_profiles/registry.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/Space-DF/transformer-service/internal/device_profiles/abeeway"
+	"github.com/Space-DF/transformer-service/internal/device_profiles/am307"
 	"github.com/Space-DF/transformer-service/internal/device_profiles/common"
 	"github.com/Space-DF/transformer-service/internal/device_profiles/rak2270"
 	"github.com/Space-DF/transformer-service/internal/device_profiles/rak4630"
@@ -28,6 +29,7 @@ func RegisterAll(r *Component) error {
 		parser       common.Parser
 	}{
 		{abeeway.Model, abeeway.Manufacturer, abeeway.NewAbeewayComponent()},
+		{am307.Model, am307.Manufacturer, am307.NewAM307Component()},
 		{rak2270.Model, rak2270.Manufacturer, rak2270.NewRAK2270Component()},
 		{rak4630.Model, rak4630.Manufacturer, rak4630.NewRAK4630Component()},
 		{rak7200.Model, rak7200.Manufacturer, rak7200.NewRAK7200Component()},
