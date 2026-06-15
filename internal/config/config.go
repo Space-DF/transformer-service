e1e068089a2a75751d0cb7d8fc873f074d24ff8c
lethanhdat762003 <dat.le@df.technology>
2026-06-15 16:35:46 +0700

    fix: resolve comments

--- internal/config/config.go
+++ internal/config/config.go
@@ -118,7 +118,7 @@ func New() (Config, error) {
 func setDefaults(vp *viper.Viper) {
 	vp.SetDefault("server.log_level", "info")
 	vp.SetDefault("amqp.broker_url", "amqp://default:${RABBITMQ_DEFAULT_PASS}@rabbitmq:5672/")
-	vp.SetDefault("amqp.output_topics", []string{"tenant.*.transformed.device.location", "tenant.*.transformed.telemetry.device.location"})
+	vp.SetDefault("amqp.output_topics", []string{"tenant.*.broker.device.location", "tenant.*.telemetry.device.location"})
 	vp.SetDefault("amqp.consumer_tag", "transformer-service")
 	vp.SetDefault("amqp.prefetch_count", 10)
 	vp.SetDefault("amqp.auto_ack", false)
